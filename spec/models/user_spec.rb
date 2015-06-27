95f646d4aa9311b3dfb915968188e8bfa43cff20
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2015-06-27 12:35:49 +0200

    Rename projects to owned projects

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 describe User do
   it { should validate_presence_of(:email) }
-  it { should have_many(:projects).with_foreign_key('owner_id') }
+  it { should have_many(:owned_projects).with_foreign_key('owner_id') }
 end
