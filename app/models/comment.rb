342f6bfa4fbc9741e7498220cd969d653312b22c
karcia <karciawow@gmail.com>
2015-06-27 11:23:37 +0200

    Add migration and relation to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ActiveRecord::Base
+  has_one :user
+  belongs_to :project
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150627091156) do
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "project_id"
   end
 
   create_table "projects", force: :cascade do |t|
