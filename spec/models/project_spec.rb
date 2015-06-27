352b02c9a723d98d1b0d533fa31d4e6ddeccd512
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-06-27 12:09:23 +0200

    repair project spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Project do
-	it { should belong_to(:user) }
+	it { should belong_to(:owner) }
 	it { should have_many(:comments) }
 	it { should have_many(:rewards) }
 	it { should have_and_belong_to_many(:tags) }
