f1d6e88484193ac7e1e511330b04d3003df41d4a
Marek Łukaszek <lukaszek.marek@gmail.com>
2015-06-27 11:14:34 +0200

    Add Project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627090144) do
+ActiveRecord::Schema.define(version: 20150627091156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
