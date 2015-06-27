0f92d610247bc76514df7d93711a42121bd20fb8
karcia <karciawow@gmail.com>
2015-06-27 14:15:49 +0200

    Add validation in user model; setup devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe User do
   it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:name) }
   it { should have_many(:owned_projects).with_foreign_key('owner_id') }
   it { should have_and_belong_to_many(:backed_projects).with_foreign_key('backer_id') }
 end
