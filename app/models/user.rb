ed48562e6a5285718b2360165a539d7a22ff9a05
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2015-06-27 15:12:32 +0200

    Fix relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, 
-         :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
-  has_many :owned_projects, class: 'Project', foreign_key: :owner_id
-  has_and_belongs_to_many :backed_projects, class: 'Project', join_table: :backed_projects, foreign_key: :backer_id
+  has_many :owned_projects, class_name: 'Project', foreign_key: :owner_id
+  has_and_belongs_to_many :backed_projects, class_name: 'Project', join_table: :backed_projects, foreign_key: :backer_id
   has_many :comments
 
   validates_presence_of :email, :name
