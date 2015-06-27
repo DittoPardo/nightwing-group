ce6d29c8bba5c787b3989d179be47dbbbbbe3253
Marcin Ostrowski <marcin.ostrowski@netguru.pl>
2015-06-27 15:52:00 +0200

    Fix model. fix seeds

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,8 @@ class Project < ActiveRecord::Base
   has_many :comments
   has_many :rewards
   has_and_belongs_to_many :tags
-  has_and_belongs_to_many :backers, class: 'User', join_table: :backed_projects, foreign_key: :backed_project_id
-  belongs_to :owner, class: 'User'
+  has_and_belongs_to_many :backers, class_name: 'User', join_table: :backed_projects, foreign_key: :backed_project_id
+  belongs_to :owner, class_name: 'User'
 
   validates_presence_of :owner
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
-require 'faker'
-user = FactoryGirl.create(:user)
+user = User.create(
+  email: 'test@test.com',
+  password: 'q1w2e3r4',
+  password_confirmation: 'q1w2e3r4',
+  name: 'John Doe'
+)
 
 5.times do |n|
-  Project.create!(
-    name: Faker::App.name,
-    description: Faker::Lorem.paragraphs,
-    user: user
+  Project.create(
+    name: "Test project #{n+1}",
+    description: 'Long long long description',
+    owner: user
   )
 end
