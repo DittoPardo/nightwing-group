208892b243d9a641130cc8c2371c6d65a417f4d4
Marek Łukaszek <lukaszek.marek@gmail.com>
2015-06-27 15:01:26 +0200

    Add facebook authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :projects
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   root 'welcome#index'
 
