8479415a7182aef6c188e0843f42926bbb9da06d
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-06-28 16:04:50 +0200

    authenticate project owner

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :authenticate_owner!, only: [:edit, :update, :destroy]
+
   expose_decorated(:project, attributes: :project_params)
   expose_decorated(:projects) { q.result }
   expose(:comments) { project.comments }
@@ -32,6 +34,11 @@ class ProjectsController < ApplicationController
   end
 
   private
+  def authenticate_owner!
+    unless current_user == project.owner
+      redirect_to project_url(project), flash: { error: 'You are not allowed to do that.' }
+    end
+  end
 
   def project_params
     params.require(:project).permit(:name, :description, :tag_list, :photo)
