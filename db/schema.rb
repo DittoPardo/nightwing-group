613ac58045bc572bdf6c3dfd58f5361299dd130c
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-06-28 13:06:32 +0200

    add photo column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628102636) do
+ActiveRecord::Schema.define(version: 20150628105812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150628102636) do
     t.integer  "owner_id"
     t.string   "name"
     t.text     "description"
+    t.string   "photo"
   end
 
   create_table "projects_tags", force: :cascade do |t|
