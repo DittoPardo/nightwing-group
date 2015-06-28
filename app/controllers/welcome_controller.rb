5f7dd1a8861439dcd255b086f7aa85a4f6505706
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-06-28 16:33:32 +0200

    frontpage improvement

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,3 @@
 class WelcomeController < ApplicationController
-  expose_decorated(:popular_projects, decorator: ProjectDecorator) { Project.order("RANDOM()").limit(3) }
+  expose_decorated(:latest_projects, decorator: ProjectDecorator) { Project.order("created_at DESC").limit(3) }
 end
